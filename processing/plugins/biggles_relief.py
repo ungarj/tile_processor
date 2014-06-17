451f7e796a8e4ea2fa80a6b2925445e2a8b0450e
Joachim Ungar <joachim.ungar@eox.at>
2014-06-17 10:52:19 +0200

    further biggles testing

--- processing/plugins/biggles_relief.py
+++ processing/plugins/biggles_relief.py
@@ -14,6 +14,18 @@ def process(parsed, target, temp_metatile, temp_processed, save_offsetx, save_of
     scale = parsed.s
     zfactor = parsed.z
     altitude = parsed.alt
+    
+    _, gt, _, nodata, array_metatile = numpy_read(temp_metatile)
+    #os.remove(temp_metatile)
+
+    filtered_numpy = ndimage.median_filter(array_metatile, size=3)
+    array_metatile = []
+    array_metatile = ndimage.median_filter(filtered_numpy, size=5)
+    filtered_numpy = []
+    filtered_numpy = ndimage.median_filter(array_metatile, size=3)
+
+    numpy_save(filtered_numpy, temp_metatile, 0, 0, filtered_numpy.shape[0], filtered_numpy.shape[1], gt, nodata, ot)
+
     process_hillshade = "gdaldem hillshade -s %s -z %s -alt %s %s -of GTiff %s > /dev/null" %(scale, zfactor, altitude, temp_metatile, temp_processed)
     os.system(process_hillshade)
     nodata = 0
@@ -21,4 +33,15 @@ def process(parsed, target, temp_metatile, temp_processed, save_offsetx, save_of
     # open in numpy
     _, gt, _, nodata, array_numpy = numpy_read(temp_processed)
     processed_numpy = ndimage.median_filter(array_numpy, size=3)
+    array_numpy = []
+    array_numpy = ndimage.median_filter(processed_numpy, size=3)
+    processed_numpy = []
+    processed_numpy = ndimage.median_filter(array_numpy, size=3)
+    array_numpy = []
+    array_numpy = ndimage.median_filter(processed_numpy, size=3)
+    processed_numpy = []
+    processed_numpy = ndimage.median_filter(array_numpy, size=3)
+
     numpy_save(processed_numpy, target, save_offsetx, save_offsety, save_xsize, save_ysize, gt, nodata, ot)
+
+    #os.remove(temp_metatile)
